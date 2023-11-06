de67777ba6db611b6007fdbff96f461a81ea83c1
tohasim <toreh.simonsen@live.dk>
2023-11-06 13:59:31 +0100

    Init commit

---
+++ src/main/java/com/example/soulshelperbackend/SoulsHelperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.soulshelperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulsHelperBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoulsHelperBackendApplication.class, args);
+    }
+
+}
